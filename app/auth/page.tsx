3bbc32a97f9421d10274323b2f1de731cc3211d1
Allen Zhou <46854522+allenzhou101@users.noreply.github.com>
2024-04-18 19:31:21 -0700

    Update page.tsx

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -5,7 +5,7 @@ const Page = () => {
         <div className="flex flex-col items-center p-24 rounded-md">
             <Descope
                 flowId="sign-up-or-in"
-                redirectAfterSuccess="/dasboard"
+                redirectAfterSuccess="/dashboard"
                 // redirectAfterError="/error-page"
             />
         </div>
