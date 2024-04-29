f3ce867c91d5a2e10716e0131f7b6b8cd689f8fc
Allen Zhou <46854522+allenzhou101@users.noreply.github.com>
2024-04-29 11:04:19 -0700

    Update route.ts

--- app/api/assistant/route.ts
+++ app/api/assistant/route.ts
@@ -4,14 +4,6 @@ import { AssistantResponse } from 'ai';
 // IMPORTANT! Set the runtime to edge
 export const runtime = 'edge';
  
-const homeTemperatures = {
-  bedroom: 20,
-  'home office': 21,
-  'living room': 21,
-  kitchen: 22,
-  bathroom: 23,
-};
- 
 export async function POST(req: Request) {
   // Parse the request body
   const input: {
@@ -41,72 +33,8 @@ export async function POST(req: Request) {
       });
  
       // forward run status would stream message deltas
-      let runResult = await forwardStream(runStream);
- 
-      // status can be: queued, in_progress, requires_action, cancelling, cancelled, failed, completed, or expired
-      while (
-        runResult?.status === 'requires_action' &&
-        runResult.required_action?.type === 'submit_tool_outputs'
-      ) {
-        const tool_outputs =
-          runResult.required_action.submit_tool_outputs.tool_calls.map(
-            (toolCall: any) => {
-              const parameters = JSON.parse(toolCall.function.arguments);
- 
-              switch (toolCall.function.name) {
-                case 'getRoomTemperature': {
-                  const temperature =
-                    homeTemperatures[
-                      parameters.room as keyof typeof homeTemperatures
-                    ];
- 
-                  return {
-                    tool_call_id: toolCall.id,
-                    output: temperature.toString(),
-                  };
-                }
- 
-                case 'setRoomTemperature': {
-                  const oldTemperature =
-                    homeTemperatures[
-                      parameters.room as keyof typeof homeTemperatures
-                    ];
- 
-                  homeTemperatures[
-                    parameters.room as keyof typeof homeTemperatures
-                  ] = parameters.temperature;
- 
-                  sendDataMessage({
-                    role: 'data',
-                    data: {
-                      oldTemperature,
-                      newTemperature: parameters.temperature,
-                      description: `Temperature in ${parameters.room} changed from ${oldTemperature} to ${parameters.temperature}`,
-                    },
-                  });
- 
-                  return {
-                    tool_call_id: toolCall.id,
-                    output: `temperature set successfully`,
-                  };
-                }
- 
-                default:
-                  throw new Error(
-                    `Unknown tool call function: ${toolCall.function.name}`,
-                  );
-              }
-            },
-          );
- 
-        runResult = await forwardStream(
-          openai.beta.threads.runs.submitToolOutputsStream(
-            threadId,
-            runResult.id,
-            { tool_outputs },
-          ),
-        );
-      }
+      await forwardStream(runStream);
+      
     },
   );
 }
